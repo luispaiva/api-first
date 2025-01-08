73e8d7b968ca58abac315b17f585df71592dc0ad
Luis Paiva <contato@luispaiva.com.br>
2025-01-08 15:45:22 -0300

    Adicionado novas classes no carregamento.

--- functions.php
+++ functions.php
@@ -8,4 +8,6 @@ if (file_exists(__DIR__ . '/vendor/autoload.php')) {
     require_once __DIR__ . '/vendor/autoload.php';
 }
 
+App\Api\Attachment::getInstance();
+App\Api\Auth::getInstance();
 App\Api\Service::getInstance();
